47c19a72d92e1c26a40b09e32f1ceeb046a7b980
Sahil-Bista <160755798+Sahil-Bista@users.noreply.github.com>
2024-07-20 13:51:20 +0545

    2nd commit

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -32,6 +32,19 @@ let getRandomUser = () => {
 
 
 //inserting new data
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+let data=[]
+for(let i=1; i <=100 ;i++){
+  data.push(getRandomUser()); 
+}
+try{
+  connection.query(q,[data],(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+  });
+}catch(err){
+  console.log(err);
+}
 
 
 app.get("/", (req,res)=>{
